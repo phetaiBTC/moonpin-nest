9645029e97f9d667fb94fe5f1b77dd70b8a3472c
unknown <phetaibtc@gmail.com>
2025-05-07 11:29:27 +0700

    update

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -8,6 +8,15 @@ export class User {
     @Column()
     username: string;
 
-    @Column()
+    @Column({ unique: true })
     email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
 }
